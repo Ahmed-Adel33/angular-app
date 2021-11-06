2e77c1643eb91037f8f2bf0e8fd11d04f1dd4414
Ahmed-Adel33 <ahmed920adel@gmail.com>
2021-11-06 17:25:35 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,32 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'todo';
+  addItem(description: string) {
+	this.allItems.unshift({
+	  description,
+	  done: false
+	});
+  }
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'Javscript', done: true },
+    { description: 'python', done: false },
+    { description: 'angular', done: false },
+    { description: 'react', done: false },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
+  }
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoAppComponent } from './todo-app/todo-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoAppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
